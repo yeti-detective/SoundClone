6fc10a530fd75af9e299fade241470620481d723
Christopher Brown <yeti.detective@gmail.com>
2018-05-11 17:42:37 -0400

    fixed some janky logic in song_show

--- frontend/components/pages/song_show.jsx
+++ frontend/components/pages/song_show.jsx
@@ -11,25 +11,28 @@ export default class SongShow extends Component {
 
   render () {
 
-    return ( (this.props.songs[this.props.thisSongId] && (
-      <main className="song-show">
-        <UserHeaderBar />
-        <section
-          className="song-banner"
-          style={{background: 'linear-gradient(45deg, #959595, #555555)'}}
-          >
-          <img src={this.props.songs[this.props.thisSongId].image_url} />
-          <h1 className="main-song-title">{this.props.songs[this.props.thisSongId].title}</h1>
-          <h3 className="artist-name">{this.props.users[this.props.songs[this.props.thisSongId].user_id].username}</h3>
-        </section>
-        <section className="song-cousins">
-          {listifySliceOfState(this.props.songs).map((song) => {
-            return song.id != this.props.thisSongId ? (
-              <p>{song.title}</p>
-            ) : ( null );
-          })}
-        </section>
-      </main> )) || null
-    );
+    if (this.props.songs[this.props.thisSongId]) {
+      return (
+        <main className="song-show">
+          <UserHeaderBar />
+          <section
+            className="song-banner"
+            style={{background: 'linear-gradient(45deg, #959595, #555555)'}}
+            >
+            <img src={this.props.songs[this.props.thisSongId].image_url} />
+            <h1 className="main-song-title">{this.props.songs[this.props.thisSongId].title}</h1>
+            <h3 className="artist-name">{this.props.users[this.props.songs[this.props.thisSongId].user_id].username}</h3>
+          </section>
+          <section className="song-cousins">
+            {listifySliceOfState(this.props.songs).map((song) => {
+              return song.id != this.props.thisSongId ? (
+                <p>{song.title}</p>
+              ) : ( null );
+            })}
+          </section>
+        </main> );
+    } else {
+      return null;
+    }
   }
 }
