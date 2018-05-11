db1c35d2d45c68856484137e5cedc64775f3407b
Christopher Brown <yeti.detective@gmail.com>
2018-05-11 09:54:20 -0400

    Updated validation for background image attachment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  validate :username, :session_token, :password_digest
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   has_attached_file :avatar, default_url: 'default_icon.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :background_image, default_url: 'background_images/default_background.png'
-  # validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, default_url: 'background_images/default_background.png'
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   before_validation :ensure_session_token
   attr_reader :password
