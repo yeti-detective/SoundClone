a32ec10a079c9085ffc507f7fd774c971395dc16
Christopher Brown <yeti.detective@gmail.com>
2018-05-11 13:02:51 -0400

    sorting out database migrations

--- db/migrate/20180510132923_add_art_to_song.rb
+++
@@ -1,5 +0,0 @@
-class AddArtToSong < ActiveRecord::Migration[5.1]
-  def change
-    add_column :songs, :song_image_url, :text
-  end
-end
--- db/migrate/20180510133958_drop_songs_table.rb
+++
@@ -1,5 +0,0 @@
-class DropSongsTable < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :songs
-  end
-end
