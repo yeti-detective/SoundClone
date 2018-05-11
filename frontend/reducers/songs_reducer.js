f1e8f57cd01cd1ab1b929d2d04ac6cef359928c5
Christopher Brown <yeti.detective@gmail.com>
2018-05-11 12:19:23 -0400

    one bug in songs reducer

--- frontend/reducers/songs_reducer.js
+++ frontend/reducers/songs_reducer.js
@@ -6,7 +6,7 @@ const songsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_SONG:
-      return merge({}, state, { [action.song.id]: song});
+      return merge({}, state, { [action.song.id]: action.song});
     case RECEIVE_SONGS:
       return merge({}, state, action.songs);
     default:
