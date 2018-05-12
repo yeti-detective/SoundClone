11a527c26e62c311f978918587dbd4be2e945e06
Christopher Brown <yeti.detective@gmail.com>
2018-05-12 11:47:30 -0400

    continued styling of landing page

--- frontend/components/small_components/song_badge.jsx
+++ frontend/components/small_components/song_badge.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 const SongBadge = (props) => (
   <figure className="song-badge">
+    <Link to={`/songs/${props.song.id}`}>
     <img src={props.song.image_url} />
     <h3>{props.song.title}</h3>
     <h4>{props.user}</h4>
+    </Link>
   </figure>
 );
 
