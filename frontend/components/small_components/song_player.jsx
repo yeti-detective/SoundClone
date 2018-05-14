d162732cd1002fb1109b77d151fc0382f0d1eb39
Christopher Brown <yeti.detective@gmail.com>
2018-05-14 10:34:14 -0400

    removed one source tag that was superfluous, it turns out

--- frontend/components/small_components/song_player.jsx
+++ frontend/components/small_components/song_player.jsx
@@ -13,7 +13,6 @@ export default class SongPlayer extends Component {
     return (
       <figure className="song-player">
         <audio controls>
-          <source src={this.props.song.file_path} type="audio/3gp" />
           <source src={this.props.song.file_path} type="audio/mpeg" />
           <source src={this.props.song.file_path} type="audio/ogg" />
           Your browser does not support HTML5 audio, what the heck?
