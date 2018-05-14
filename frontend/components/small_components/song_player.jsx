fbd7bf0b4a9303e342665e4058dfc2a9ce270d1d
Christopher Brown <yeti.detective@gmail.com>
2018-05-14 14:18:09 -0400

    removed useless componentWillMount method

--- frontend/components/small_components/song_player.jsx
+++ frontend/components/small_components/song_player.jsx
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 export default class SongPlayer extends Component {
 
-  componentDidMount() {
-    const songPlayer = document.getElementsByClassName('song-player')[0];
-    songPlayer.addEventListener('play', (e) => {
-      console.log(e);
-    });
-  }
 
   render() {
     return (
