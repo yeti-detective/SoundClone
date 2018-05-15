93ff16043d2d9af8addaf322758c8250a36afaa6
student <aa@aa.com>
2018-05-15 12:57:44 -0400

    restoring song show's audio player while fixing footer bar's audio player

--- frontend/components/containers/song_show_container.js
+++ frontend/components/containers/song_show_container.js
@@ -5,10 +5,7 @@ import { getSong, getUsersSongs } from '../../actions/songs_actions';
 import { getUser } from '../../actions/users_actions';
 import SongShow from '../pages/song_show';
 
-import listifySliceOfState from '../../util/listify_slice_of_state';
-
 const mapStateToProps = (state, ownProps) => {
-  // const songs = listifySliceOfState(state.entities.songs);
   return {
     thisSongId: ownProps.match.params.songId,
     songs: state.entities.songs || {},
@@ -17,7 +14,6 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  // debugger
   return {
     getSong: (id) => dispatch(getSong(id)),
     getUser: () => dispatch(getUser(ownProps.match.params.userId)),
