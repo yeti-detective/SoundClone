bde05b9d10bb3eb8ac86b318c3f2d84c14e9c151
Christopher Brown <yeti.detective@gmail.com>
2018-05-16 16:40:54 -0400

    removed link as it is superfluous

--- frontend/components/small_components/song_card_tool_tip.jsx
+++ frontend/components/small_components/song_card_tool_tip.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const SongCardToolTip = props => (
   <div className="tooltip">
-    <button>Add To Currently Playing</button>
-    <Link to={`/users/${props.song.user_id}/${props.song.id}`}>Go to Page</Link>
+    <button
+      onClick={() => { props.getCurrentSong(props.song.id); }}>
+      Add To Currently Playing
+    </button>
   </div>
 );
 
