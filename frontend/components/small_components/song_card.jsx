bc6a0004508ed0fedd13f40d75bdd348e4a4ddc9
Christopher Brown <yeti.detective@gmail.com>
2018-05-16 15:37:04 -0400

    added tooltip for song manipulation

--- frontend/components/small_components/song_card.jsx
+++ frontend/components/small_components/song_card.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SongCardToolTip from './song_card_tool_tip';
 
 const SongCard = (props) => {
 
   return (
     <div onClick={() => {props.getSong(props.song);}} className="song-card">
+      <SongCardToolTip song={props.song} />
       <Link to={`/users/${props.song.user_id}/${props.song.id}`}>
         <img className="song-icon" src={props.song.image_url} />
         <p>
