f3b684335a865d9188388262774ec5a86ba76989
Christopher Brown <yeti.detective@gmail.com>
2018-05-17 11:36:14 -0400

    added new get current song component to song badge card

--- frontend/components/small_components/song_badge_card.jsx
+++ frontend/components/small_components/song_badge_card.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import SongPlayer from './song_player';
 import SongBadge from './song_badge';
+import GetCurrentSongButton from '../containers/get_current_song_button_container';
 
 const SongBadgeCard = (props) => {
   return (
     <figure className="song-card-mid" >
       <SongBadge song={props.song} user={props.user.username} />
+      <GetCurrentSongButton song={props.song} />
     </figure>
   );
 };
 
 export default SongBadgeCard;
-
-// <SongPlayer song={props.song} />
