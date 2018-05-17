a8ee31baed70b6964ec4998916345e277a5feab9
Christopher Brown <yeti.detective@gmail.com>
2018-05-17 10:26:38 -0400

    fixing 'each child should have a unique key prop' issue in song card index

--- frontend/components/small_components/song_card_index.jsx
+++ frontend/components/small_components/song_card_index.jsx
@@ -5,6 +5,9 @@ const SongCardIndex = props => {
   return (
     <div className="song-card-index">
       {props.songs.map((song) => {
+        if (!song.id) {
+          song.id = 1;
+        }
         return <SongCard key={song.id} song={song} getCurrentSong={props.getCurrentSong} />;
       })}
     </div>
