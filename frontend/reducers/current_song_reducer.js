1621bdd2e0144a89115246687dcb68cd23a0ce97
Christopher Brown <yeti.detective@gmail.com>
2018-05-17 10:49:08 -0400

    adjustment to reducer to receive new action structure

--- frontend/reducers/current_song_reducer.js
+++ frontend/reducers/current_song_reducer.js
@@ -4,7 +4,7 @@ const currentSongReducer = (state = {}, action ) => {
   Object.freeze(state);
   switch(action.type) {
     case 'RECEIVE_CURRENT_SONG':
-      return action.song[Object.keys(action.song)[0]];
+      return action.song.song[Object.keys(action.song.song)[0]];
     default:
       return state;
   }
