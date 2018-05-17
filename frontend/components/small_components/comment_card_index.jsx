790fbc32b2be5c2fd5812955ad31af2dcd248511
Christopher Brown <yeti.detective@gmail.com>
2018-05-17 19:08:54 -0400

    making those comments pretty

--- frontend/components/small_components/comment_card_index.jsx
+++ frontend/components/small_components/comment_card_index.jsx
@@ -5,12 +5,13 @@ import CommentCard from './comment_card';
 const CommentCardIndex = props => {
   return (
     <ul>
+      <li><h3>comments</h3></li>
       {props.comments.map((comment) => {
         if (comment.song_id === props.song.id) {
           return (
             <CommentCard
               key={comment.id}
-              author={props.users[comment.user_id]} 
+              author={props.users[comment.user_id]}
               comment={comment}
               />
           );
