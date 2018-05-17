49d0862a7c1d092a0181c95ac936c5efe2c9d4b6
yeti-detective <yeti.detective@gmail.com>
2018-05-17 07:42:02 -0400

    added show method and view as well as partial

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,25 @@
 class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
   def index
     @comments = Comment.all
   end
-  
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :song_id)
+  end
+
 end
--- app/views/api/comments/index.json.jbuilder
+++ app/views/api/comments/index.json.jbuilder
@@ -1,5 +1,5 @@
 @comments.each do |comment|
   json.set! comment.id do
-    json.extract! comment, :id, :user_id, :song_id, :body
+    json.partial! 'api/comments/comment', comment: comment
   end
 end
