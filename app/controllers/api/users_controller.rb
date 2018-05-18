777cbeaa34022b5ee54cba8be6e57ad898fd76fa
yeti-detective <yeti.detective@gmail.com>
2018-05-17 23:26:16 -0400

    updated user controller create method to reflect new state shape

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,6 +12,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in(@user)
+      @songs = @user.songs
       render :show
     else
       render json: @user.errors.full_messages, status: 422
