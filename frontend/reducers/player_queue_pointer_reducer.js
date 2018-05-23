220241bec9fa14cec05b3bbed7f65e85f15ad325
yeti-detective <yeti.detective@gmail.com>
2018-05-23 16:30:22 -0400

    added play queue pointer to state

---
+++ frontend/reducers/player_queue_pointer_reducer.js
@@ -0,0 +1,13 @@
+import {
+  INCREMENT_POINTER,
+  DECREMENT_POINTER
+ } from '../actions/pointer_actions';
+
+const playQueuePointerReducer = (state = 0, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default playQueuePointerReducer;
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
 import currentSongReducer from './current_song_reducer';
 import playQueueReducer from './play_queue_reducer';
+import playQueuePointerReducer from './player_queue_pointer_reducer';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
   playQueue: playQueueReducer,
+  playQueuePointer: playQueuePointerReducer,
   currentSong: currentSongReducer
 });
 
