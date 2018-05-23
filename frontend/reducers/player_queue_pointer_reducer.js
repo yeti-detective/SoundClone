214637a5d5ecb4979ba90b97c0be9cfbc2669c04
yeti-detective <yeti.detective@gmail.com>
2018-05-23 17:57:25 -0400

    completed play queue reducer and pointer reducer

--- frontend/reducers/player_queue_pointer_reducer.js
+++ frontend/reducers/player_queue_pointer_reducer.js
@@ -2,9 +2,16 @@ import {
   INCREMENT_POINTER,
   DECREMENT_POINTER
  } from '../actions/pointer_actions';
+ import { CLEAR_SONG_QUEUE } from '../actions/songs_actions';
 
 const playQueuePointerReducer = (state = 0, action) => {
   switch(action.type) {
+    case INCREMENT_POINTER:
+      const incState = state + 1;
+      return incState;
+    case DECREMENT_POINTER:
+      const decState = state - 1;
+      return decState;
     default:
       return state;
   }
