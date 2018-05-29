cb8e042e6022c3a4282c567f1cc53b7cb1183ecc
yeti-detective <yeti.detective@gmail.com>
2018-05-29 12:28:38 -0400

    current song reducer remvoed, root reducer updated to reflect new state shape

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,18 +1,19 @@
 import { combineReducers } from 'redux';
 
 import entitiesReducer from './entities_reducer';
-import currentSongReducer from './current_song_reducer';
 import playQueueReducer from './play_queue_reducer';
 import playQueuePointerReducer from './player_queue_pointer_reducer';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
+import playingReducer from './playing_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer,
   playQueue: playQueueReducer,
-  playQueuePointer: playQueuePointerReducer
+  playQueuePointer: playQueuePointerReducer,
+  playing: playingReducer
 });
 
 export default rootReducer;
