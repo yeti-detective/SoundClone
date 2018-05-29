5c8dad5d4f953cecdba20fc7663af10e8ef1e84d
yeti-detective <yeti.detective@gmail.com>
2018-05-29 12:03:28 -0400

    updated reducer to reflect new approach to app state

--- frontend/reducers/play_queue_reducer.js
+++ frontend/reducers/play_queue_reducer.js
@@ -1,5 +1,5 @@
 import {
-  RECEIVE_SONG,
+  ENQUEUE_SONG,
   REMOVE_SONG_FROM_QUEUE,
   CLEAR_SONG_QUEUE
  } from '../actions/songs_actions';
@@ -7,13 +7,13 @@ import {
 const playQueueReducer = (state = [], action) => {
   Object.freeze(state);
   switch(action.type) {
-    case RECEIVE_SONG:
+    case ENQUEUE_SONG:
       const newState = Object.assign([], state);
-      newState.push(Object.keys(action.song.song)[0])
+      newState.push(action.id);
       return newState;
     case REMOVE_SONG_FROM_QUEUE:
       let remState = Object.assign([], state);
-      const idx = remState.indexOf(String(action.id))
+      const idx = remState.indexOf(String(action.id));
       debugger
       remState = remState.slice(0, idx).concat(remState.slice(idx + 1, remState.length));
       return remState;
