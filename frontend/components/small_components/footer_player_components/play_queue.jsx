2cc14d5944cdb3a28e3cd9d677a18219d163eb6b
yeti-detective <yeti.detective@gmail.com>
2018-05-29 00:07:18 -0400

    refactored so unique keys can be obtained even if song is enqueued twice

--- frontend/components/small_components/footer_player_components/play_queue.jsx
+++ frontend/components/small_components/footer_player_components/play_queue.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import QueueCard from './queue_card';
 
 const PlayQueue = (props) => {
-
+  let cardKeys = 0;
   return (
     <ul className={`play-queue ${props.show ? 'show-queue' : null}`}>
       <h3>Next Up</h3>
       {
         props.queue.map (
-          (song) => (
-            <QueueCard
-              key={props.songs[song].id || 1}
-              song={props.songs[song]}
-              />
-          )
+          (song) => {
+            return (
+              <QueueCard
+                key={cardKeys++}
+                song={props.songs[song]}
+                />
+            )
+          }
         )
       }
     </ul>
