100e656233343874c1dfd64b100350735cb45339
yeti-detective <yeti.detective@gmail.com>
2018-06-03 17:10:58 -0400

    including PgSearch module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  include PgSearch
+  multisearchable against: [:username]
+
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
