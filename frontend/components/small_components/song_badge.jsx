a0130e212f973a1d66a4f04c1748fbe613f439a3
yeti-detective <yeti.detective@gmail.com>
2018-06-27 16:48:39 -0400

    updated to prettier formatting

--- frontend/components/small_components/song_badge.jsx
+++ frontend/components/small_components/song_badge.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import FaComment from 'react-icons/lib/fa/comment';
 
-const SongBadge = (props) => {
+const SongBadge = props => {
   // TODO: find out why the comments array is coming in double
   const numComments = new Set(props.song.comments);
   return (
     <figure className="song-badge">
       <Link to={`/users/${props.song.user_id}/${props.song.id}`}>
-      <img src={props.song.image_url} />
-      <h3>{props.song.title}</h3>
-      <h4>{props.user}</h4>
-      <FaComment className="comment-icon" />{numComments.size}
+        <img src={props.song.image_url} />
+        <h3>{props.song.title}</h3>
+        <h4>{props.user}</h4>
+        <FaComment className="comment-icon" />
+        {numComments.size}
       </Link>
     </figure>
   );
