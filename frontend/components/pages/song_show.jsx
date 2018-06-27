16abed75bb3d9924f8e48d18cd2704abf8605ead
yeti-detective <yeti.detective@gmail.com>
2018-06-27 16:44:35 -0400

    removing turbolinks

--- frontend/components/pages/song_show.jsx
+++ frontend/components/pages/song_show.jsx
@@ -17,51 +17,63 @@ export default class SongShow extends Component {
     window.scrollTo(0, 0);
   }
 
-  artistName () {
-    return this.props.users[this.props.songs[this.props.thisSongId].user_id].username;
+  artistName() {
+    return this.props.users[this.props.songs[this.props.thisSongId].user_id]
+      .username;
   }
 
-  backgroundImg () {
-    return this.props.users[this.props.songs[this.props.thisSongId].user_id].background_image;
+  backgroundImg() {
+    return this.props.users[this.props.songs[this.props.thisSongId].user_id]
+      .background_image;
   }
 
-
-  render () {
+  render() {
     if (this.props.songs[this.props.thisSongId]) {
       return (
         <main className="app song-show">
           <UserHeaderBar />
           <section
             className="song-banner"
-            style={{backgroundImage: `url(${this.backgroundImg()})`}}
-            >
+            style={{ backgroundImage: `url(${this.backgroundImg()})` }}>
             <div className="song-info-box">
               <img
                 className="song-logo-large"
                 src={this.props.songs[this.props.thisSongId].image_url}
-                />
-              <h1 className="main-song-title">{this.props.songs[this.props.thisSongId].title}</h1>
+              />
+              <h1 className="main-song-title">
+                {this.props.songs[this.props.thisSongId].title}
+              </h1>
               {!emptyOb(this.props.users) ? (
-                <Link to={"/users/" + this.props.songs[this.props.thisSongId].user_id}>
+                <Link
+                  to={
+                    '/users/' + this.props.songs[this.props.thisSongId].user_id
+                  }>
                   <h3 className="artist-name">{this.artistName()}</h3>
                 </Link>
-              ) : null }
-              <GetCurrentSongButton song={this.props.songs[this.props.thisSongId]} />
+              ) : null}
+              <GetCurrentSongButton
+                song={this.props.songs[this.props.thisSongId]}
+              />
             </div>
-            </section>
+          </section>
           <section>
             <h2>More by this user:</h2>
             <section className="song-cousins">
               <SongCardIndex
-                user={this.props.users[this.props.songs[this.props.thisSongId].user_id]}
+                user={
+                  this.props.users[
+                    this.props.songs[this.props.thisSongId].user_id
+                  ]
+                }
                 songs={listifySliceOfState(this.props.songs)}
-                />
+              />
               <CommentCardIndex
                 song={this.props.songs[this.props.match.params.songId]}
-                />
+              />
             </section>
           </section>
-        </main> );
+        </main>
+      );
     } else {
       return null;
     }
