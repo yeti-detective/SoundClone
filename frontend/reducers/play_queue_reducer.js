a597e1a9a033114f9b1b5bc6e1c71c8bc80d012d
yeti-detective <yeti.detective@gmail.com>
2018-06-27 18:13:37 -0400

    prettier formatting and removed debugger

--- frontend/reducers/play_queue_reducer.js
+++ frontend/reducers/play_queue_reducer.js
@@ -2,11 +2,11 @@ import {
   ENQUEUE_SONG,
   REMOVE_SONG_FROM_QUEUE,
   CLEAR_SONG_QUEUE
- } from '../actions/songs_actions';
+} from '../actions/songs_actions';
 
 const playQueueReducer = (state = [], action) => {
   Object.freeze(state);
-  switch(action.type) {
+  switch (action.type) {
     case ENQUEUE_SONG:
       const newState = Object.assign([], state);
       newState.push(action.id);
@@ -14,14 +14,15 @@ const playQueueReducer = (state = [], action) => {
     case REMOVE_SONG_FROM_QUEUE:
       let remState = Object.assign([], state);
       const idx = remState.indexOf(String(action.id));
-      debugger
-      remState = remState.slice(0, idx).concat(remState.slice(idx + 1, remState.length));
+      remState = remState
+        .slice(0, idx)
+        .concat(remState.slice(idx + 1, remState.length));
       return remState;
     case CLEAR_SONG_QUEUE:
       return [];
     default:
       return state;
   }
-}
+};
 
 export default playQueueReducer;
